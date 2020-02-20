ae3d41d6b5df2587d031f76cc02b8d888ff5f27d
Ivan Tananaev <ivan.tananaev@gmail.com>
2020-02-20 19:15:18 +0300

    feat: функции контроллера

--- interface/controller/refresh_controller.go
+++ interface/controller/refresh_controller.go
@@ -1,7 +1,22 @@
 package controller
 
+import (
+	"github.com/WiMank/MoonWriterService/interface/response"
+	"github.com/WiMank/MoonWriterService/usecase"
+)
+
 type refreshController struct {
+	interactor usecase.RefreshInteractor
 }
 
 type RefreshController interface {
+	RefreshTokens() response.AppResponse
+}
+
+func NewRefreshController(interactor usecase.RefreshInteractor) usecase.RefreshInteractor {
+	return &refreshController{interactor}
+}
+
+func (rc *refreshController) RefreshTokens() response.AppResponse {
+	return &response.UnauthorizedResponse{}
 }
